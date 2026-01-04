d15e7bfb32e3c831f8faa2b79439a03b4139ecae
Fengnanahub <108163503+Fengnanahub@users.noreply.github.com>
2026-01-04 13:15:13 +0800

    Update writebackMgz_toMgz.py

--- writebackMgz_toMgz.py
+++ writebackMgz_toMgz.py
@@ -6,13 +6,13 @@ import pandas as pd
 import numpy as np
 
 # ==== 输入部分 ====
-# 你的 aparc+aseg 模板 (subject 的 mri 目录里)
+#aparc+aseg 模板 (subject/mri)
 aparc_aseg_file = f".../write_loading_back_25xin/aparc.a2009s+aseg.mgz"
 
-# 你的 ROI 结果表格 (CSV, 必须包含 ROI 名称和数值两列)-------------------- need revise----------------
+# CSV, ROI name+value
 roi_csv = f".../allSubgroup4_y166roi_loadingORG.csv"
 
-# 输出文件 ---------------------- need revise---------------------
+# output
 output_file = f".../write_loading_back_25xin/subgroup4_back_Loading_all.mgz"
 
 # ==== 读取数据 ====
@@ -21,8 +21,6 @@ data = img.get_fdata().astype(np.float32) #get template data
 
 roi_df = pd.read_csv(roi_csv) # get loading文件
 # roi_df.iloc[:, 2:6] = roi_df.iloc[:, 2:6].astype(np.float32) # 第3-6列的loading 数据格式为float 32; 防止freesurfer可视化时候报错；
-# print(roi_df.dtypes) # 转换无效
-
 
 # CSV应至少有两列: "ROI" (名称, 如 "Left-Hippocampus") 和 "Value" (统计值)
 # ROI 名称要和 FreeSurferColorLUT 中的 label 名称一致
@@ -41,7 +39,7 @@ with open(lut_file, "r") as f:
         idx, name = int(parts[0]), parts[1]
         lut_dict[name] = idx
 
-# ==== 创建结果体积 ====
+# ==== create result====
 result_data = np.zeros_like(data)
 
 for roi_name, value in roi_dict.items():
@@ -52,21 +50,18 @@ for roi_name, value in roi_dict.items():
     mask = data == label_id
     result_data[mask] = value
 
-# ==== 保存新体积 ====
+# ==== save ====
 result_img = nib.Nifti1Image(result_data, affine=img.affine, header=img.header)
 nib.save(result_img, output_file) # write back
 
 print(f"✅ Done! Result saved to {output_file}")
-print("👉 你可以用以下命令查看结果：")
-print(f"freeview -v {output_file}:colormap=heat:opacity=0.6 {aparc_aseg_file}:opacity=0.2")
 
-
-############## 可视化写回的mgz文件
+############## visualize 
 from nilearn import plotting
 
 img = nib.load(output_file)
 
-# 可视化之前，转换数据格式folat 64 为float 32;
+# 转换数据格式folat 64 为float 32;
 data = img.get_fdata()
 data = data.astype('float32')
 new_img = nib.Nifti1Image(data, img.affine)
@@ -76,4 +71,4 @@ saveHTML_path = r".../write_loading_back_25xin/subgroup4_back_Loading_all.html"
 
 view = plotting.view_img(new_img, threshold=None)
 view.save_as_html(saveHTML_path)
-print("结果已保存为{saveHTML_path}，用浏览器打开即可查看")
+
